08a6053d189177bd515f2de831683d3747d6c27c
Sophia Damm <sophiadammzogaibmardones@gmail.com>
2026-01-09 13:07:29 -0300

    bug(terminal) : sintaxe  para reservar espaço para o vetor

--- u2/sophiadamm_202400018378_terminal.s
+++ u2/sophiadamm_202400018378_terminal.s
@@ -1,82 +1,58 @@
+#Infos gerais:
+#a0 -> base da uart (RHR/THR) 
+#a1 -> endereço do vetor
+#t0 -> acumulador
 
-
-# Code section
+#Code section
 .section .text
-# printf function
-printf:
-    # printf loop
-    printf_loop:
-        # Reading string character
-        lb t0, 0(a1)
-        # Checking for null character
-        beq t0, zero, printf_end
-        # Outputting character
-        sb t0, 0(a0)
-        # Incrementing index
-        addi a1, a1, 1
-        # Branching to loop
-        j printf_loop
-    # printf end
-    printf_end:
-        # Returning from call
-        ret
-
-# scanf function
-scanf:
-    # Scan loop
-    scanf_loop:
-        # Reading string character
-        lb t0, 0(a0)
-        # Storing character
-        sb t0, 0(a1)
-        # Incrementing index
-        addi a1, a1, 1
-        # Retrieving Data Ready fom Line Status Register (LSR)
-        lb t0, 5(a0)
-        andi t0, t0, 1
-        # Branching to loop
-        bne t0, zero, scanf_loop
-    # Appending null character in string
-    sb zero, 0(a1)
-    # Returning from call
-    ret
 
 # Main function
 .global main
 main:
-    # Prologue
     addi sp, sp, -16
     sw ra, 0(sp)
-    # Setting UART0 address
-    li a0, 0x10000000
-    # Setting string address
-    la a1, input
-    # Calling scanf function
-    call scanf
-    # Setting string address
-    la a1, input
-    # Calling printf function
-    call printf
-    # Setting string address
-    la a1, hello_world
-    # Calling printf function
-    call printf
-    # Epilogue
+
+    li a0, 0x10000000      # base da uart (valor do inteiro)  
+    la a1, vetor           # la -> load addres
+
+    jal ra, ler_inteiro
+
     lw ra, 0(sp)
     addi sp, sp, 16
-    # Setting return value to zero (success)
     li a0, 0
-    # Returning from call
     ret
 
-# Read-only data section
-.section .rodata
-# Hello world string
-hello_world:
-    .string "Hello world!\n"
+
+# Polling - com esperas
+ler_inteiro:
+    li t0, 0            # acumulador
+
+loop_leitura: 
+    # Verificar se tem o dado - LSR (deslocamento de 5 da base)
+    lb t1, 5(a0)        
+    andi t1, t1, 1      # isola o bit 0 (Data Ready)
+    beq t1, zero, loop_leitura  # Se for 0, não tem dado. Volta e espera.
+
+    # Ler o dado de RHR
+    lb t2, 0(a0)       
+
+    li t3, 32           # ASCII do Espaço
+    beq t2, t3, fim_leitura
+    li t3, 10           # ASCII do "\n" e valor para multiplicar dps
+    beq t2, t3, fim_leitura
+
+    addi t2, t2, -48    # aiscii -> inteiro
+
+    mul t0, t0, t3   
+    add t0, t0, t2 
+
+    j loop_leitura
+
+fim_leitura:
+    mv a0, t0           # Move o resultado final (t0) para o registrador de retorno (a0)
+    ret                 # Volta para a main
 
 # Data section
 .section .data
-# Input string (99 characters allocated + '\0')
-input:
-    .zero 100
+vetor:
+    .zero 4000      
