eb28caa43a9f30274efd9943324fe6be609c60c5
Sophia Damm <sophiadammzogaibmardones@gmail.com>
2026-01-09 14:15:52 -0300

    feat(terminal): epílogo e prólogo realistas

--- u2/terminal.s
+++ u2/terminal.s
@@ -14,6 +14,10 @@
 main:
     addi sp, sp, -32
     sw ra, 0(sp)
+    sw s0, 4(sp)            
+    sw s1, 8(sp)            
+    sw s2, 12(sp)           
+    sw s3, 16(sp)          
 
     li s0, 0x10000000     # base da uart (valor do inteiro - retorno)  
     jal ra, ler_inteiro
@@ -44,7 +48,11 @@ loop_preencher_vetor:
 fim_programa:
     # --- EPILOGO ---
     lw ra, 0(sp)
-    addi sp, sp, 32
+    lw s0, 4(sp)            
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    addi sp, sp, 32         
     li a0, 0
     ret
 
